6a224475213f52310960f83bc44376ac42f45f2d
Krzysiek Karczewski <krzysztofkarczewski92@gmail.com>
2019-12-15 20:30:00 +0100

    fix registration error handling

--- routes/register.js
+++ routes/register.js
@@ -5,7 +5,7 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const config = require("config");
 
-router.get("/", function(req, res) {
+router.get("/", function (req, res) {
   res.render("register.pug");
 });
 
@@ -14,21 +14,29 @@ router.post("/", async (req, res) => {
     email: req.body.email
   });
 
-  if (user) res.render("register.pug", { error: "User already exists." });
+  if (user) res.render("register.pug", {
+    error: "User already exists."
+  });
 
   user = new User({
     firstName: req.body.firstName,
     lastName: req.body.lastName,
     email: req.body.email,
     password: req.body.password
-  });
+  })
 
+  try {
+    await user.save();
+  } catch (err) {
+    res.render("register.pug", {
+      errors: err.errors
+    })
+  }
   const salt = await bcrypt.genSalt(10);
   user.password = await bcrypt.hash(user.password, salt);
-  await user.save();
+  await user.save()
 
-  const token = jwt.sign(
-    {
+  const token = jwt.sign({
       _id: this._id
     },
     config.get("jwtPrivateKey")
