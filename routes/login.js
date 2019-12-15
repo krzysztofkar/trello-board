e75adde7a2ec433ab73ec1f1e7a16de3e0078296
Krzysiek Karczewski <krzysztofkarczewski92@gmail.com>
2019-12-15 15:51:19 +0100

    add basic registration and login logic

--- routes/login.js
+++ routes/login.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
 
 router.get("/", function(req, res) {
   res.render("login.pug");
 });
 
-router.post("/", function(req, res) {
+router.post("/", async (req, res) => {
+  let user = await User.findOne({
+    email: req.body.email
+  });
+
+  if (!user) return res.status(400).send("Invalid email or password.");
+
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
+  if (!validPassword) return res.status(400).send("Invalid email or password.");
+
   res.redirect("/api/tasks");
 });
 
--- routes/register.js
+++ routes/register.js
@@ -1,11 +1,30 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
 
 router.get("/", function(req, res) {
   res.render("register.pug");
 });
 
-router.post("/", function(req, res) {
+router.post("/", async (req, res) => {
+  let user = await User.findOne({
+    email: req.body.email
+  });
+
+  if (user) return res.status(400).send("User already exists.");
+
+  user = new User({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    password: req.body.password
+  });
+
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
+  await user.save();
+
   res.redirect("/api/login");
 });
 
